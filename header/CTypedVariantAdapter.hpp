93f55a212728b598e6dff00aeae5c5da99e781af
I Putu Gede Darmawan S.Kom <ipgd_bali@yahoo.com>
2020-07-30 00:59:53 +0800

    Removing CInt & CUInt and adding CTypedVariantAdapter

---
+++ header/CTypedVariantAdapter.hpp
@@ -0,0 +1,18 @@
+#ifndef CTYPEDVARIANTADAPTER_HPP
+#define CTYPEDVARIANTADAPTER_HPP
+
+#include "CTypedVariant.hpp"
+
+class CTypedVariantAdapter
+{
+	private:
+		CTypedVariant & m_TypedVariant;
+
+	protected:
+		CTypedVariantAdapter(CTypedVariant & typedVariant,size_t IdTypedVariant);
+
+	public:
+
+}
+
+#endif
---
+++ source/CTypedVariantAdapter.cpp
@@ -0,0 +1,9 @@
+#include "CTypedVariantAdapter.hpp"
+
+CTypedVariantAdapter::CTypedVariantAdapter(
+		CTypedVariant typedVariant,
+		size_t IdTypedVariand)
+	: m_TypedVariant(typedVariant)
+{
+
+}
